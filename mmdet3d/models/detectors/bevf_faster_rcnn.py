78ddae5e808f68443014b6569b4f9b5d4d4d629d
ZJUnewer <2251596077@qq.com>
2024-12-11 15:15:19 +0800

    fusion with lidar depth

--- mmdet3d/models/detectors/bevf_faster_rcnn.py
+++ mmdet3d/models/detectors/bevf_faster_rcnn.py
@@ -50,6 +50,7 @@ class BEVF_FasterRCNN(MVXFasterRCNN):
         self.camera_depth_range = camera_depth_range
         self.lift = camera_stream
         self.se = se
+        self.pc_range = pc_range
         if camera_stream:
             self.lift_splat_shot_vis = LiftSplatShoot(lss=lss, grid=grid, inputC=imc, camC=64, 
             pc_range=pc_range, final_dim=final_dim, downsample=downsample)
@@ -99,31 +100,102 @@ class BEVF_FasterRCNN(MVXFasterRCNN):
     
     def extract_feat(self, points, img, img_metas, gt_bboxes_3d=None):
         """Extract features from images and points."""
+        img_size = img.size() 
         img_feats = self.extract_img_feat(img, img_metas)
         pts_feats = self.extract_pts_feat(points, img_feats, img_metas)
-
         if self.lift:
             BN, C, H, W = img_feats[0].shape
-            batch_size = BN//self.num_views
+            batch_size = BN // self.num_views
             img_feats_view = img_feats[0].view(batch_size, self.num_views, C, H, W)
             rots = []
             trans = []
+            rots_depth = []
+            trans_depth = []
             for sample_idx in range(batch_size):
                 rot_list = []
                 trans_list = []
+                rot_depth_list = []
+                trans_depth_list = []
                 for mat in img_metas[sample_idx]['lidar2img']:
-                    mat = torch.Tensor(mat).to(img_feats_view.device)
-                    rot_list.append(mat.inverse()[:3, :3])
-                    trans_list.append(mat.inverse()[:3, 3].view(-1))
+                    mat = torch.Tensor(mat)
+                    rot_list.append(mat.inverse()[:3, :3].to(img_feats_view.device))
+                    trans_list.append(mat.inverse()[:3, 3].view(-1).to(img_feats_view.device))
+                    rot_depth_list.append(mat[:3, :3].to(img_feats_view.device))
+                    trans_depth_list.append(mat[:3, 3].view(-1).to(img_feats_view.device))
                 rot_list = torch.stack(rot_list, dim=0)
                 trans_list = torch.stack(trans_list, dim=0)
+                rot_depth_list = torch.stack(rot_depth_list, dim=0)
+                trans_depth_list = torch.stack(trans_depth_list, dim=0)
                 rots.append(rot_list)
                 trans.append(trans_list)
+                rots_depth.append(rot_depth_list)
+                trans_depth.append(trans_depth_list)
             rots = torch.stack(rots)
             trans = torch.stack(trans)
+            rots_depth = torch.stack(rots_depth)  # depth transform 4 6 3 3
+            trans_depth = torch.stack(trans_depth)  # depth transform
+
+            # 创建缩放矩阵
+            scale_x = H / img_size[3]
+            scale_y = W / img_size[4]
+            scale_matrix = torch.tensor([
+                [scale_x, 0, 0],
+                [0, scale_y, 0],
+                [0, 0, 1]
+            ]).cuda()
+            rots_depth_scaled = torch.matmul(scale_matrix, rots_depth)
+            trans_depth_scaled = trans_depth * torch.tensor([scale_x, scale_y, 1]).cuda()
             lidar2img_rt = img_metas[sample_idx]['lidar2img']  #### extrinsic parameters for multi-view images
-            
+
+            batch_size = len(points)
+            dx = int((self.camera_depth_range[1] - self.camera_depth_range[0]) / self.camera_depth_range[2])
+            depth = torch.zeros(batch_size, img_size[1], 1, dx, H, W).cuda() # 创建大小
+            depth_mask = torch.zeros(batch_size, img_size[1], 1, H, W).cuda() # 创建大小
+            for b in range(batch_size):
+                cur_coords = points[b].float()[:, :3]  #取点的xyz
+                # lidar2image
+                cur_coords = rots_depth_scaled[b].matmul(cur_coords.transpose(1, 0))
+                cur_coords += trans_depth_scaled[b].reshape(-1, 3, 1)
+
+                # get 2d coords
+                dist = cur_coords[:, 2, :]
+                cur_coords[:, 2, :] = torch.clamp(cur_coords[:, 2, :], 1e-4, 1e4)
+                cur_coords[:, :2, :] /= cur_coords[:, 2:3, :]
+                # imgaug
+
+                cur_coords = cur_coords[:, :2, :].transpose(1, 2)
+                # normalize coords for grid sample
+                cur_coords = cur_coords[..., [1, 0]]
+
+                on_img = (
+                    (cur_coords[..., 0] < H)
+                    & (cur_coords[..., 0] >= 0)
+                    & (cur_coords[..., 1] < W)
+                    & (cur_coords[..., 1] >= 0)
+                )
+
+                for c in range(on_img.shape[0]):
+                    masked_coords = cur_coords[c, on_img[c]].long()  # 点云投影到图像坐标
+                    masked_dist = dist[c, on_img[c]]  # 对应深度
+
+                    # 使用字典存储每个坐标的深度值
+                    coord_dict = {}
+                    for i in range(masked_coords.shape[0]):
+                        coord = (masked_coords[i, 0].item(), masked_coords[i, 1].item())
+                        if coord not in coord_dict:
+                            coord_dict[coord] = []
+                        coord_dict[coord].append(masked_dist[i].item())
+
+                    # 计算每个坐标的深度均值并更新 depth 和 depth_mask
+                    for coord, img_depths in coord_dict.items():
+                        depth_mask[b, c, 0, coord[0], coord[1]] = 1  # 有效的像素点
+                        for img_depth in img_depths:
+                            if img_depth < self.camera_depth_range[0] or img_depth > self.camera_depth_range[1]:
+                                continue
+                            depth[b, c, 0, int((img_depth-self.camera_depth_range[0]) / self.camera_depth_range[2]), coord[0], coord[1]] += 1 / len(img_depths)  # 稀疏的深度约束图（用于计算loss）
+            #TODO, 已完成depth 作为离散分布真值的计算  depth：torch.size(B, N, D, H, W)
             img_bev_feat, depth_dist = self.lift_splat_shot_vis(img_feats_view, rots, trans, lidar2img_rt=lidar2img_rt, img_metas=img_metas)
+
             # print(img_bev_feat.shape, pts_feats[-1].shape)
             if pts_feats is None:
                 pts_feats = [img_bev_feat] ####cam stream only
